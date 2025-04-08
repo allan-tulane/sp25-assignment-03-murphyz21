9ef9ce5e1b8c40c0319c266aba09541e2a253031
Zoe Murphy <zmurphy@tulane.edu>
2025-04-07 23:52:21 -0500

    final

--- answers.md
+++ answers.md
@@ -1,8 +1,50 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:**__Zoe Murphy_______________________
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+**1a)** Given a $N$ dollars, state a greedy algorithm for producing
+as few coins as possible that sum to $N$.
+    Start with a list of empty coins. 
+    When N (the amount of coins) is greater than 0
+        Look for the biggest 2^k that is less than or equal to N
+        subtract 2^k from N
+        add the coin value 2^k to that initial list
+    return the list
+
+**1b)** Prove that this algorithm is optimal by proving the greedy
+  choice and optimal substructure properties.
+    This algorithm has a greedy choice because it's choosing the largest possible coin that doesn't exceed the remaining amount. All the coins are powers of 2 and a smaller combo of coins would require more coins than just taking the largest one, so it's efficient. 
+    It has an optimal substructure because taking the largest coin and subtracting it from the total creates a smaller version of the same problem over and over. 
+
+**1c)** What is the work and span of your algorithm?
+    O(log(n)) work and O(log n) span
+
+**2a)** You realize the greedy algorithm you devised above doesn't
+  work in Fortuito. Give a simple counterexample that shows that the
+  greedy algorithm does not produce the fewest number of coins.
+    You could have coins that are worth 1, 5, and 7 dollars and you want 10 dollars. 
+    The greedy algorithm would do the 7 dollar coin and then three 1 dollar coins. (4 coins total)
+    It'd be better to just do two 5 dollar coins.
+
+**2b)** Since you paid attention in Algorithms class, you realize that
+  while this problem does not have the greedy choice property it does
+  have an optimal substructure property. State and prove this
+  property.
+    This has optimal substructure because we have to solve the smaller coin problems first. The subproblems have to be solved first. 
+    You can like find the minimum coins needed for amount N by adding one coin k to the optimal solution N - k. This shows that N depends on the solutions to the smaller subproblems. 
+
+  **2c)** Use this optimal substructure property to design a
+  dynamic programming algorithm for this problem. If you used top-down
+  or bottom-up memoization to avoid recomputing solutions to
+  subproblems, what is the work and span of your approach?
+    You can have a list that looks at the minimum coins needed from 0 - N
+    Then for each N, check the coins and update the list with the smallest number of coins needed
+    then return the list
+
+    work is O(n*k) because N is like the target amount and k is the number of coins and we're checking each coin for each amount up to N
+
+    span is O(n) cause it's like proportional to the number of amounts N
--- main.py
+++ main.py
@@ -8,22 +8,77 @@ alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev-
 
 def MED(S, T):
     # TO DO - modify to account for insertions, deletions and substitutions
+    # If length of first string is empty, return length of second string
     if (S == ""):
-        return(len(T))
+        return (len(T))
+    # If second string is empty, return length of first string
     elif (T == ""):
-        return(len(S))
+        return (len(S))
     else:
+        # If first characters match, move to the next character
         if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+            return (MED(S[1:], T[1:]))
+        # Else, take the minimum of the three possible options and add 1 for the operation
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
-
+            return (1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+    # if result is like already there just return it
+    if (S, T) in MED:
+        return MED[(S, T)]
+    # If T is empty return length of S
+    if T == "":
+        return len(S)
+    # If S is empty return length of T
+    if S == "":
+        return len(T)
+
+    # If the first characters match, move to next character
+    elif S[0] == T[0]:
+        MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
+    # Else calculate cost for insertion and deletion and take minimum
+    else:
+        insert_cost = fast_MED(S, T[1:], MED)  
+        delete_cost = fast_MED(S[1:], T, MED)  
+        
+        # Store the result: add 1 for the operation (insert or delete) and take the minimum cost
+        MED[(S, T)] = 1 + min(insert_cost, delete_cost)
+    # Return stored result for the pair
+    return MED[(S, T)]
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    # if result is already compute
+    if (S, T) in MED:
+        return MED[(S, T)]
 
+    # If T is empty add S with -
+    if T == "":
+        MED[(S, T)] = (S, "-" * len(S))
+        return MED[(S, T)]
+    # If S is empty add T with -
+    elif S == "":
+        MED[(S, T)] = ("-" * len(T), T)
+        return MED[(S, T)]
+
+    # If first characters match, just align and move to the next characters
+    if S[0] == T[0]:
+        align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        MED[(S, T)] = (S[0] + align_S, T[0] + align_T)
+    # Else, Calculate the alignment for inserton and deletion
+    else:
+        insertion_S, insertion_T = fast_align_MED(S, T[1:], MED)
+        deletion_S, deletion_T = fast_align_MED(S[1:], T, MED)
+
+        # Calculate lengths of insertion and deletions
+        insertion_C = 1 + len(insertion_S)
+        deletion_C = 1 + len(deletion_S)
+
+        # If insertion is cheaper or equal add the gap to S and move forward in T
+        if insertion_C <= deletion_C:
+            MED[(S, T)] = ("-" + insertion_S, T[0] + insertion_T)
+        # Else, do it the otheer way (add gap to T and move forward in S)
+        else:
+            MED[(S, T)] = (S[0] + deletion_S, "-" + deletion_T)
+    # Return final alignment
+    return MED[(S, T)]
